fd281afbdbd825ce21b652f5fb66dc21a14eff5c
Sepia <wz45855283@163.com>
2020-09-04 15:17:57 +0800

    初识提交

---
+++ built/tsc.js
@@ -0,0 +1,19 @@
+var name = "Gene";
+var age = 37;
+var sentence = "Hello, my name is " + name + ".\nI'll be " + (age + 1) + " years old next month.";
+document.body.innerHTML = sentence;
+var Student = (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Jane", "M.", "User");
+document.body.innerHTML = greeter(user);
+//# sourceMappingURL=tsc.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+let name: string = `Gene`;
+let age: number = 37;
+let sentence: string = `Hello, my name is ${ name }.
+I'll be ${ age + 1 } years old next month.`
+
+document.body.innerHTML = sentence;
